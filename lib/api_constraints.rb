05039cce3f6925c7666677cc10676704cf6b9819
mukeshrj <er.mukeshsharmaa.rj21@gmail.com>
2016-02-12 18:12:16 +0530

    new changes

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,9 @@ class ApiConstraints
   end
 
   def matches?(req)
-    @default || req.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
+    @default || req.headers['Accept'].include?("application/vnd.rest_api.v#{@version}")
   end
 end
+
+# curl -H 'Accept: application/vnd.rest_api.v1' \
+#             http://api.localhost:3000/users
